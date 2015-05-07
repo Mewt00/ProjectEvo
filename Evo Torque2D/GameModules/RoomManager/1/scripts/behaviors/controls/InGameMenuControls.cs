15b694e6181749671ce06e2e97c9a60ecbeebc7f
Mewt00 <bealscs@mail.uc.edu>
2015-05-07 12:50:55 -0400

    Added an esc during game back to menu

---
+++ Evo Torque2D/GameModules/RoomManager/1/scripts/behaviors/controls/InGameMenuControls.cs
@@ -0,0 +1,68 @@
+//-----------------------------------------------------------------------------
+// Basic GlobalControlBehavior
+//-----------------------------------------------------------------------------
+
+if (!isObject(InGameMenuControlsBehavior))
+{
+	echo("inGameMenuActionMap");
+    %template = new BehaviorTemplate(InGameMenuControlsBehavior);
+	
+	if(isObject(inGameMenuActionMap))
+	{
+		echo("inGameMenuActionMap being popped and deleted");
+		inGameMenuActionMap.pop();
+		inGameMenuActionMap.delete();
+	}
+	
+	//new ActionMap(inGameMenuActionMap);
+	
+    %template.friendlyName = "Menu Controls";
+    %template.behaviorType = "Input";
+    %template.description  = "Exiting basic control";
+
+    %template.addBehaviorField(exitKey, "Key to bind to game exit", keybind, "keyboard escape");
+}
+
+function InGameMenuControlsBehavior::onBehaviorAdd(%this)
+{
+	echo("ingame object?: " @ isObject(inGameMenuActionMap));
+	
+    if (!isObject(inGameMenuActionMap))
+       return;
+   
+   	echo("ingame object?: " @ getWord(%this.exitKey, 0));
+
+	
+    inGameMenuActionMap.bindObj(getWord(%this.exitKey, 0), getWord(%this.exitKey, 1), "exitGame", %this);
+	
+	inGameMenuActionMap.push();
+	
+	echo("InGameMenuControlsBehavior.onBehaviorAdd()");
+}
+
+function InGameMenuControlsBehavior::onBehaviorRemove(%this)
+{
+    if (!isObject(inGameMenuActionMap))
+       return;
+
+    //%this.owner.setUpdateCallback(false);
+
+    inGameMenuActionMap.unbindObj(getWord(%this.exitKey, 0), getWord(%this.exitKey, 1), %this);
+	
+	//inGameMenuActionMap.pop();
+	inGameMenuActionMap.delete();
+}
+
+function InGameMenuControlsBehavior::exitGame(%this, %val)
+{
+	if(%val == 1)
+	{
+		echo("RoomManager.exitGame()");
+		mainWindow.delete();
+		inGameMenuActionMap.pop();
+		//inGameMenuActionMap.delete();
+		alxStop($roomMusicHandle);
+		Canvas.pushDialog(MenuDialog);
+		$titleMusicHandle = alxPlay("GameAssets:mainMenuMusic");
+	}
+}
